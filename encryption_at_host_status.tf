c24e209e41f9687702a3d3a5edb7b89d43601136
maneesha kollu <86995927+mkollu5559@users.noreply.github.com>
2025-01-13 09:45:46 -0600

    Update encryption_at_host_status.tf

--- encryption_at_host_status.tf
+++ encryption_at_host_status.tf
@@ -14,3 +14,21 @@ resource "azurerm_provider_registration" "compute" {
 output "encryption_at_host_status" {
   value = azurerm_subscription_feature.encryption_at_host.state
 }
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "null_resource" "enable_encryption_at_host" {
+  provisioner "local-exec" {
+    command = <<EOT
+      az feature register --namespace "Microsoft.Compute" --name "EncryptionAtHost"
+      az provider register --namespace "Microsoft.Compute"
+    EOT
+  }
+  triggers = {
+    always_run = timestamp()
+  }
+}
+terraform import azurerm_resource_provider_registration.compute /subscriptions/<subscription_id>/providers/Microsoft.Compute
