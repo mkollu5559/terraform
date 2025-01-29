ea0865097f1d7e68ef56292c02b1f1c40b437c32
maneesha kollu <86995927+mkollu5559@users.noreply.github.com>
2025-01-29 12:10:11 -0600

    Update k8.tf

--- k8.tf
+++ k8.tf
@@ -2,45 +2,60 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_policy_definition" "trusted_registry_policy" {
-  name         = "trusted-container-registries-policy"
-  policy_type  = "Custom"
-  mode         = "Indexed"
-  display_name = "Enforce Trusted Container Registries"
-  description  = "Ensures container images are pulled from trusted registries."
-  
-  policy_rule = <<POLICY_RULE
-  {
-    "if": {
-      "allOf": [
-        {
-          "field": "type",
-          "equals": "Microsoft.ContainerRegistry/registries"
-        },
-        {
-          "field": "Microsoft.ContainerRegistry/registries/loginServer",
-          "notIn": [
-            "mytrustedregistry.azurecr.io",
-            "anothertrustedregistry.azurecr.io"
-          ]
-        }
-      ]
-    },
-    "then": {
-      "effect": "Deny"
-    }
-  }
-  POLICY_RULE
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = "East US"
+  resource_group_name = "myResourceGroup"
+  dns_prefix          = "myaks"
 
-  metadata = <<METADATA
-  {
-    "category": "Kubernetes"
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
   }
-  METADATA
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_policy_assignment" "trusted_registry_assignment" {
-  name                 = "trusted-container-registries-assignment"
-  scope                = azurerm_kubernetes_cluster.example.id
-  policy_definition_id = azurerm_policy_definition.trusted_registry_policy.id
+resource "azurerm_policy_definition" "allowed_images" {
+  name         = "kubernetes-allowed-images"
+  policy_type  = "Custom"
+  mode         = "Microsoft.Kubernetes"
+  display_name = "Allowed Container Images"
+
+  metadata = <<METADATA
+    {
+      "category": "Kubernetes"
+    }
+  METADATA
+
+  policy_rule = <<POLICY_RULE
+    {
+      "if": {
+        "allOf": [
+          {
+            "field": "kubernetes.pod.spec.containers[*].image",
+            "notMatchInsensitively": "^(${join("|", [
+              "crnovaxregistryshared\\.azurecr\\.io",
+              "docker\\.io/bitnami",
+              "ghcr\\.io",
+              "registry\\.k8s\\.io",
+              "quay\\.io/jetstack"
+            ])}).*"
+          }
+        ]
+      },
+      "then": {
+        "effect": "Deny"
+      }
+    }
+  POLICY_RULE
+}
+
+resource "azurerm_policy_assignment" "allowed_images_assignment" {
+  name                 = "allowed-images-assignment"
+  policy_definition_id = azurerm_policy_definition.allowed_images.id
+  scope                = azurerm_kubernetes_cluster.aks.id
 }
