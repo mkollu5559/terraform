0275477a5ef0f2bd4f817004432ea20bcbeb9942
maneesha kollu <86995927+mkollu5559@users.noreply.github.com>
2025-02-17 12:38:14 -0600

    Update Azure AD Application Registration.tf

--- Azure AD Application Registration.tf
+++ Azure AD Application Registration.tf
@@ -1,22 +1,34 @@
-# Fetch Microsoft Graph API's application ID dynamically
-data "azuread_application_published_app_ids" "well_known" {}
+provider "azuread" {}
 
-data "azuread_application" "graph" {
-  application_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
-}
-
-# Fetch Permission ID for "User.Read.All"
+# Fetch Microsoft Graph API service principal
 data "azuread_service_principal" "graph" {
-  object_id = data.azuread_application.graph.object_id
+  display_name = "Microsoft Graph"
 }
 
+# Fetch the permission ID for "User.Read.All"
 data "azuread_service_principal_delegated_permission" "user_read" {
   service_principal_id = data.azuread_service_principal.graph.object_id
   claim_value          = "User.Read.All"
 }
+
+# Create an Azure AD Application Registration
+resource "azuread_application" "v1_1_app" {
+  display_name = "V1.1 Monitoring Integration"
+}
+
+# Grant API Permissions dynamically (No Hardcoding)
 resource "azuread_application_api_permission" "graph_read" {
   application_object_id = azuread_application.v1_1_app.object_id
-  api_client_id         = data.azuread_application.graph.application_id
+  api_client_id         = data.azuread_service_principal.graph.application_id
   permission_ids        = [data.azuread_service_principal_delegated_permission.user_read.id]
   type                 = "Application"
+}
+
+# Output values
+output "app_id" {
+  value = azuread_application.v1_1_app.client_id
+}
+
+output "object_id" {
+  value = azuread_application.v1_1_app.object_id
 }
