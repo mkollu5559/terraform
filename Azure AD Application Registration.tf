46cc1b8046eea6c7378af390d80739a3f62c80f7
maneesha kollu <86995927+mkollu5559@users.noreply.github.com>
2025-02-20 15:49:27 -0600

    Update Azure AD Application Registration.tf

--- Azure AD Application Registration.tf
+++ Azure AD Application Registration.tf
@@ -1,38 +1,29 @@
-resource "azuread_application" "app" {
-  display_name = "my-app-registration"
-
-  api {
-    oauth2_permission_scope {
-      admin_consent_description  = "Allow the application to read data"
-      admin_consent_display_name = "Read access"
-      id                         = "read-scope-id"
-      is_enabled                 = true
-      type                       = "User"
-      user_consent_description   = "Allow the application to read your data"
-      user_consent_display_name  = "Read access"
-      value                      = "read"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
-resource "azuread_app_role_assignment" "role_assignment" {
-  app_role_id         = "role-id" # Replace with the actual role ID
-  principal_object_id = azuread_application.app.object_id
-  resource_object_id  = "resource-object-id" # Replace with the object ID of the resource (e.g., another app or service)
+# Define the custom role for App Registration Read
+resource "azurerm_role_definition" "app_registration_reader" {
+  name        = "App Registration Reader"
+  scope       = "/"
+  description = "Allows read-only access to Azure AD application registrations."
+
+  permissions {
+    actions = [
+      "microsoft.directory/applications/standard/read",
+      "microsoft.directory/applications/basic/read"
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    "/"
+  ]
 }
 
-resource "azuread_application_api_access" "api_access" {
-  application_object_id = azuread_application.app.object_id
-  api_permission {
-    id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # Microsoft Graph - User.Read
-    type = "Scope"
-  }
-}
-
-resource "azuread_application_consent" "admin_consent" {
-  application_object_id = azuread_application.app.object_id
-  api_permission {
-    id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # Microsoft Graph - User.Read
-    type = "Scope"
-  }
+# Assign the role to a user
+resource "azurerm_role_assignment" "assign_reader_role" {
+  scope              = "/"
+  role_definition_id = azurerm_role_definition.app_registration_reader.role_definition_resource_id
+  principal_id       = "USER_OR_SERVICE_PRINCIPAL_ID"
 }
